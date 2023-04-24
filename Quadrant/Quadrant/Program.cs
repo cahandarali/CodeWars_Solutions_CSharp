d87e1c45b9745cc23fee3405bd56dc535de85c5a
cahandarali <cahandarslim@gmail.com>
2023-04-24 13:55:20 +0400

    Quadrant Task Done

---
+++ Quadrant/Quadrant/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Quadrant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public static int Quadrant(int x, int y)
+        {
+            if (x > 0 && y > 0)
+            {
+                return 1;
+            }
+            else if (x < 0 && y > 0)
+            {
+                return 2;
+            }
+            else if (x < 0 && y < 0)
+            {
+                return 3;
+            }
+            else if (x > 0 && y < 0)
+            {
+                return 4;
+            }
+            return 0;
+        }
+    }
+}
