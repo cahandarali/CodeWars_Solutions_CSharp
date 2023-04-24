bca8614b0c4034bd438129a844e86b10028d2de5
cahandarali <cahandarslim@gmail.com>
2023-04-24 14:34:14 +0400

    TotalPoints done

--- CheckSameCase/CheckSameCase/Program.cs
+++ CheckSameCase/CheckSameCase/Program.cs
@@ -6,5 +6,22 @@
         {
             Console.WriteLine("Hello, World!");
         }
+        public static int SameCase(char a, char b)
+        {
+            if (!char.IsLetter(a) || !char.IsLetter(b))
+            {
+                return -1;
+            }
+            else if ((char.IsLower(a) && char.IsLower(b)) || (!char.IsLower(a) && !char.IsLower(b)))
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+
     }
 }
---
+++ TotalAmountOfPoints/TotalAmountOfPoints/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace TotalAmountOfPoints
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] a = { "1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:3" };
+            Console.WriteLine(TotalPoints(a));
+        }
+        public static int TotalPoints(string[] games)
+        {
+            int total = 0;
+            foreach (string game in games)
+            {
+                string[] score = game.Split(':');
+                int OurGoals = int.Parse(score[0]);
+                int EnemyGoals = int.Parse(score[1]);
+
+                int point = 0;
+                if(OurGoals>EnemyGoals)
+                {
+                    point = 3;
+                }
+                else if(OurGoals==EnemyGoals)
+                {
+                    point = 1;
+                }
+
+                total += point;
+            }
+            return total;
+        }
+    }
+}
+
+
+
+//{ "1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:3" }
